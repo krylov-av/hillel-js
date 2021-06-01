188c6e6a5714e8ca8a2127d039fa2f626fa1a4a1
unknown <D:\The Bat!>
2021-06-01 07:50:19 +0300

    lesson_1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+alert("Welcome! We are glad to see you!");
+let name=prompt("Please, enter your name");
+let age=prompt("Please, enter your age");
+
+alert(`Hello, ${name}, you are ${age} years old.\r\n Nice to meet you.`);
